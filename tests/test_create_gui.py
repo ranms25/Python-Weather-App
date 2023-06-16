ddcdd9d1679d7af743c10db56629294355769f07
Ran Moshe <ranmoshe68@gmail.com>
2023-06-16 16:54:14 +0300

    change test

--- tests/test_create_gui.py
+++ tests/test_create_gui.py
@@ -1,19 +1,21 @@
-import pytest
-import time
+import unittest
 from weather_app import create_gui
-from tkinter import Tk
+from unittest.mock import patch, MagicMock
 
+class TestWeatherAppGUI(unittest.TestCase):
 
-def test_create_gui():
-    duration = 10  # Set the duration in seconds
-    
-    start_time = time.time()
-    end_time = start_time + duration
-    
-    root = Tk()
-    create_gui()
-    root.after(duration * 1000, root.destroy)  # Destroy the window after the specified duration in milliseconds
-    
-    while time.time() < end_time:
-        root.update_idletasks()
-        root.update()
+    @patch('weather_app.load_dotenv')
+    @patch('weather_app.ThemedTk')
+    def test_create_gui(self, mock_ThemedTk, mock_load_dotenv):
+        # Call the create_gui() function
+        create_gui()
+
+        # Assert that the necessary GUI elements and widgets are created
+        mock_ThemedTk.assert_called_once_with(theme="Adapta")
+        mock_root = mock_ThemedTk.return_value
+        mock_root.geometry.assert_called_once_with("400x400")
+        mock_root.resizable.assert_called_once_with(0, 0)
+        mock_root.title.assert_called_once_with("Weather App")
+
+if __name__ == '__main__':
+    unittest.main()
