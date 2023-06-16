aaada16f57415dcba65eeb7102adcd0819e30aed
ranms25 <62708429+ranms25@users.noreply.github.com>
2023-06-16 16:27:41 +0300

    Update test_create_gui.py

--- tests/test_create_gui.py
+++ tests/test_create_gui.py
@@ -5,13 +5,15 @@ from tkinter import Tk
 
 
 def test_create_gui():
+    duration = 10  # Set the duration in seconds
+    
+    start_time = time.time()
+    end_time = start_time + duration
+    
     root = Tk()
     create_gui()
-
-    timeout = 5  # Set the timeout duration in seconds
-    start_time = time.time()
-
-    while time.time() - start_time < timeout:
+    root.after(duration * 1000, root.destroy)  # Destroy the window after the specified duration in milliseconds
+    
+    while time.time() < end_time:
+        root.update_idletasks()
         root.update()
-
-    root.destroy()
